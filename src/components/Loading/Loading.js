c3a057772e75b1a22139f410874efe90c4ba865b
Quyet <quyet.nguyen@aleph-labs.com>
2018-04-10 14:34:12 +0700

    Change to check

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types'
 
 import './Loading.scss'
 
-const Loading = (props) => {
-const {text} = props
+const Loading = props => {
+  const { text } = props
   return (
-    <div className="loading-container">
+    <div className="loading-container hello">
       <div className="loading" />
       <div id="loading-text">{text}</div>
     </div>
@@ -14,11 +14,11 @@ const {text} = props
 }
 
 Loading.propTypes = {
-  text: PropTypes.string
+  text: PropTypes.string,
 }
 
 Loading.defaultProps = {
-  text: 'loading'
+  text: 'loading',
 }
 
 export default Loading
